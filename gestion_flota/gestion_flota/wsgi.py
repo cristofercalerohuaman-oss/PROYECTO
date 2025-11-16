8580570c29385a5f124379393b705a98d531413b
Cristofercalerohuaman-oss <cristofercalerohuaman@ejemplo.com>
2025-11-16 00:40:18 -0500

    Renombrando 'application' a 'app' para Vercel

--- gestion_flota/gestion_flota/wsgi.py
+++ gestion_flota/gestion_flota/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_flota.settings')
 
-application = get_wsgi_application()
+# ¡CORRECCIÓN PARA VERCEL!
+# Cambiamos 'application' por 'app' para que Vercel la pueda encontrar.
+app = get_wsgi_application()
