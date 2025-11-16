ad13ecda7fcd6bee236dc85554eb79bcf9650fbd
Cristofercalerohuaman-oss <cristofercalerohuaman@ejemplo.com>
2025-11-16 01:03:31 -0500

    Arreglo final de ruta raíz 404 (moviendo 'home' a vehiculos.urls)

--- gestion_flota/gestion_flota/urls.py
+++ gestion_flota/gestion_flota/urls.py
@@ -1,7 +1,6 @@
 # gestion_flota/gestion_flota/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,13 +8,7 @@ urlpatterns = [
     # --- LOGIN/LOGOUT ---
     path('accounts/', include('django.contrib.auth.urls')), 
 
-    # --- RUTA RAÍZ (HOME) ---
-    # ¡ESTO DEBE IR PRIMERO!
-    # Redirige la raíz (/) a /dashboard/
-    path('', RedirectView.as_view(url='/dashboard/', permanent=False), name='home'),
-
-    # --- TUS APLICACIONES ---
-    # ¡ESTO DEBE IR SEGUNDO!
-    # Incluye TODAS las demás URLs de tu app (como /dashboard/, /vehiculos/, etc.)
+    # --- ¡LO ÚNICO QUE IMPORTA! ---
+    # Incluye TODAS las URLs de tu app (incluyendo la raíz)
     path('', include('vehiculos.urls', namespace='vehiculos')),
 ]
