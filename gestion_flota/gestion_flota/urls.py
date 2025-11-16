04a889e3b45d278710a9cd3c6a13e74b63ffd18a
Cristofercalerohuaman-oss <cristofercalerohuaman@ejemplo.com>
2025-11-16 01:25:16 -0500

    Arreglo final de vercel.json (añadiendo build estático)

--- gestion_flota/gestion_flota/urls.py
+++ gestion_flota/gestion_flota/urls.py
@@ -4,12 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # --- LOGIN/LOGOUT ---
     path('accounts/', include('django.contrib.auth.urls')), 
-
-    # --- ¡LO ÚNICO QUE IMPORTA! ---
-    # Incluye TODAS las URLs de tu app (incluyendo la raíz)
-    # Vercel buscará en 'vehiculos.urls' la ruta para ''
     path('', include('vehiculos.urls', namespace='vehiculos')),
 ]
